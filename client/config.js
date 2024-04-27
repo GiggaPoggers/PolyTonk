4212159686322eb61ae7b8ab2b6d21dda07bb656
Maxim sok <115567810+BigFatGrub@users.noreply.github.com>
2024-04-27 17:49:09 -0500

    Update config.js

--- client/config.js
+++ client/config.js
@@ -3,10 +3,10 @@ window.servers = [{
   wsLink: (window.top.location.protocol == "https:" ? "wss://" : "ws://") + window.top.location.host + "/server",
   name: "FFA",
 }, {
-  wsLink: "ws://polytonk.onrender.com:8080/server",
+  wsLink: "wss://polytonk2tdm.onrender.com/server",
   name: "2TDM",
 }];
 // Year, Month, Full Date
 window.changelogDate = [1712796955062];
 // SET CHANGELOG TO YOUR CURRENT DATE AFTER MAKING A CHANGE! TO GET YOUR DATE, PUT Date.now() ON YOUR CONSOLE
-window.changelog = ["Welcome to PolyTonk V1.5"];
+window.changelog = ["Welcome to PolyTonk V2"];

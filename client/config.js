b50aea564dee0e1c7eeba06dc10c6eb7add330fc
Maxim sok <115567810+BigFatGrub@users.noreply.github.com>
2024-05-03 20:06:02 -0500

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,12 +1,18 @@
 window.beta = false;
 window.servers = [{
   wsLink: (window.top.location.protocol == "https:" ? "wss://" : "ws://") + window.top.location.host + "/server",
-  name: "FFA",
+  name: "FFA ",
 }, {
   wsLink: "wss://polytonk2tdm.onrender.com/server",
-  name: "2TDM",
+  name: "2TDM ",
+}, {
+  wsLink: "wss://polytonkgrowth.onrender.com/server",
+  name: "GROWTH ",
+}, {
+  wsLink: "wss://polytonkgrowth2tdm.onrender.com/server",
+  name: "GROWTH 2TDM ",
 }];
 // Year, Month, Full Date
 window.changelogDate = [1712796955062];
 // SET CHANGELOG TO YOUR CURRENT DATE AFTER MAKING A CHANGE! TO GET YOUR DATE, PUT Date.now() ON YOUR CONSOLE
-window.changelog = ["Welcome to PolyTonk V2"];
+window.changelog = ["Welcome to PolyTonk V2.2", "Player of the month: Incognitoes", "Added two new game-modes  (GROWTH) and  (GROWTH 2TDM)", "Currently working on adding/balancing tanks.",];

ba6c1486e0c23ac866da9d3fa538834ea7da7bd7
Maxim sok <115567810+BigFatGrub@users.noreply.github.com>
2024-05-05 13:53:01 -0500

    Update config.js

--- client/config.js
+++ client/config.js
@@ -21,4 +21,4 @@ window.servers = [{
 // Year, Month, Full Date
 window.changelogDate = [1714919256888];
 // SET CHANGELOG TO YOUR CURRENT DATE AFTER MAKING A CHANGE! TO GET YOUR DATE, PUT Date.now() ON YOUR CONSOLE
-window.changelog = ["Welcome to PolyTonk V2.2", "Player of the month: Incognitoes", "Added a player chat system (press C on your keyboard to open the chat)", "Added a new game-mode (GROWTH)", "Currently working on adding/balancing tanks."];
+window.changelog = ["Welcome to PolyTonk V2.2", "Player of the month: Incognitoes", "Added 4 new tanks (Machine Gun, Destroyer, Negev, and Annihilator)", "Added a player chat system (press C on your keyboard to open the chat)", "Added a new game-mode (GROWTH)", "Currently working on adding/balancing tanks."];

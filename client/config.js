8dc09de642ade8ece5d88872d229f9c626c11c5e
Maxim sok <115567810+BigFatGrub@users.noreply.github.com>
2024-05-07 18:05:43 +0000

    Update config.js

--- client/config.js
+++ client/config.js
@@ -1,4 +1,3 @@
-window.beta = false;
 window.servers = [{
   wsLink: (function() {
     try {
@@ -7,18 +6,21 @@ window.servers = [{
       // Handle the error gracefully
       console.error("Error accessing window.top.location:", e);
       // Default to the fallback URL
-      return "wss://polytonk.com/server";
+      return "wss://polytonk-wl6i.onrender.com/server";
     }
   })(),
   name: "FFA",
-}, {
-  wsLink: "wss://polytonk2tdm.onrender.com/server",
-  name: "2TDM",
-}, {
-  wsLink: "wss://polytonkgrowth.onrender.com/server",
-  name: "GROWTH",
-}];
+}, 
+// {
+//   wsLink: "wss://polytonk2tdm.onrender.com/server",
+//   name: "2TDM",
+// }, 
+// {
+//   wsLink: "wss://polytonkgrowth.onrender.com/server",
+//   name: "GROWTH",
+// }
+];
 // Year, Month, Full Date
-window.changelogDate = [1715022867924];
+window.changelogDate = [1714919256888];
 // SET CHANGELOG TO YOUR CURRENT DATE AFTER MAKING A CHANGE! TO GET YOUR DATE, PUT Date.now() ON YOUR CONSOLE
-window.changelog = ["Welcome to PolyTonk V2.3", "Player of the month: Incognitoes", "Added 3 new tanks (Archer, Crossbow, and Cannon)", "Added a player chat system (click the button on the bottom left of the screen to open the chat)", "Added a new game-mode (GROWTH)", "Currently working on adding/balancing tanks."];
+window.changelog = ["Welcome to PolyTonk V2.3", "Player of the month: Incognitoes", "Added 8 new tanks (Machine Gun, Destroyer, Negev, Annihilator, Vanguard, Archer, Crossbow, and Cannon)", "Added a player chat system", "Removed game-modes temporaraly due to hosting issues.", "Currently working on adding/balancing tanks."];
